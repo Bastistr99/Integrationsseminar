2592e2dd130651fbcd5835b68433e4bb046db0cd
bastistr99 <seba.straub99@gmail.com>
2022-01-23 13:28:10 +0100

    Post-Api steht soweit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ResponsiveAppBar from "./components/Navbar";
 import LinearWithValueLabel from "./components/Progressbar";
 import QuestionButton from "./components/Button";
 import Fragebogen from "./components/Fragebogen";
+import JsonObjekt from "./components/Json_Objekt";
 
 function App() {
   const [progress, setProgress] = useState(1);
@@ -15,6 +16,7 @@ function App() {
       <LinearWithValueLabel value={progress > 100 ? (100) : (progress)} />
       <Fragebogen count={count - 1}/>
       <QuestionButton changeProgress={progress_button => {setProgress(progress_button)}} changeCount={fragen_counter => {setCount(fragen_counter)}} neuer_counter={count}/>
+      <JsonObjekt />
     </div>
   );
 }
---
+++ src/components/Json_Objekt.jsx
@@ -0,0 +1,76 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+const JsonObjekt = () => {
+  const get_data = () => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        product: {
+          name: "Staubsauger",
+          preis: "500",
+          art: "Haushaltsprodukt"
+        },
+        personalbezogene_aspekte: {
+          nutzer: {
+            akzeptanz: 0,
+            autonomie: 2,
+            gefahr: 3,
+            gesellschaftlich: 4,
+            komfortabel: 3,
+            medikamentenkonsum: 2,
+            privatsphaere: 1,
+            psychische_sicherheit: 3,
+            pyschische_entlastung: 1,
+            selbststaendigkeit: 2,
+            zumutbarkeit: 3
+          },
+          angehoerige: {
+            entlastung_angehoerige: 4,
+            ermoeglichung_fuehrsorge: 2
+          },
+          pflegepersonal: {
+            akzeptanz: 2,
+            entlastung: 4
+          }
+        },
+        technologische_aspekte: {
+          ausfall: 2,
+          bedienung: 4,
+          bedienungsfreundlichkeit: 2,
+          beziehung: 1,
+          datensicherheit: 0,
+          datentransparenz: 3,
+          design: 4,
+          einrichtung: 2,
+          hardware_software: 3,
+          infrastruktur: 1,
+          leistungsfaehigkeit: 1,
+          robustheit: 3,
+          schnelle_verfuegbarkeit: 1,
+          service: 2,
+          wohnraum: 2
+        },
+        ökonomische_aspekte: {
+          anschaffungskosten: 2,
+          service: 1,
+          betreuungsaufwand: 1,
+          betriebskosten: 3,
+          kostenuebernahme: 4,
+          laufende_kosten: 1,
+          wartungsaufwand: 4
+        }
+        }
+      )
+    };
+
+    fetch("http://localhost:3001/send_review", requestOptions)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
+  return <Button onClick={get_data}>Give me Data</Button>;
+};
+
+export default JsonObjekt;
