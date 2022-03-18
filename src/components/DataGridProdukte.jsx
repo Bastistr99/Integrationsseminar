5b3f753c8b618a3a187ac39488b4c815e85fd324
moritz <moritz-23@web.de>
2022-03-18 18:30:08 +0100

    Produkttabelle

--- src/components/DataGridProdukte.jsx
+++ src/components/DataGridProdukte.jsx
@@ -1,38 +1,44 @@
 import React from 'react';
 import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import { useState, useEffect } from 'react';
 
-//const rows: GridRowsProp = [
-const rows = [
-  { id: 1, col1i: 'so', col2: 'also' }
-];
 
-//const columns: GridColDef[] = [
 const columns = [
-  { field: 'col1i', headerName: 'Name', width: 200 },
-  { field: 'col2', headerName: 'Beschreibung', width: 300 },
-  { field: 'col3', headerName: 'UVP', width: 100 },
-  { field: 'col4', headerName: 'Kategorie', width: 150, },
-  { field: 'col5', headerName: 'Gesamtbewertung', width: 150, flex:1 },
+  { field: 'name', headerName: 'Name', width: 250 },
+  { field: 'beschreibung', headerName: 'Beschreibung', width: 400 },
+  { field: 'kategorie', headerName: 'Kategorie', width: 200 },
+  { field: 'uvp', headerName: 'UVP', width: 70, },
+  { field: 'bewertung', headerName: 'Gesamtbewertung', width: 150, flex:1 },
 ];
 
-function ProduktDaten() {
-  fetch("http://localhost:3001/get_produkte") 
-  .then(res => res.json())
-  .then(data => {
-    console.log(data[0].name);
-    console.log(data);
-    document.getElementById('test').innerHTML = data[0].name;
-  });
-};
-
-ProduktDaten();
 
 export default function DataGridProdukte() {
 
+  const [produkte, setProdukte] = useState ([]);
+
+  async function ProduktDaten() {
+    const data = await fetch("http://localhost:3001/get_produkte");
+    const result = await data.json();
+
+    const manipulatedResult = await result.map(({
+      _id: id,
+      ...rest
+    }) => ({
+      id,
+      ...rest
+    }));
+
+    setProdukte(manipulatedResult);
+  };
+  
+  useEffect(() => {
+   ProduktDaten();
+  }, []);
+
   return (
     <div>
-      <div style={{ height: 300, width: '80%', marginLeft: '10%', marginTop: 30}}>
-        <DataGrid rows={rows} columns={columns} />
+      <div style={{ height: 500, width: '80%', marginLeft: '10%', marginTop: 30}}>
+        <DataGrid rows={produkte} columns={columns} />
       </div>
       <div>
         <p id="test"></p>
